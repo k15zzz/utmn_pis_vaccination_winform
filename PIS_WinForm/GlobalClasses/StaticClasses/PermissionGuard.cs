d7e78fed6ba5f88ab76d8b22d91490224d5575df
Tamanit <a.plokhikh.sas@gmail.com>
2023-06-01 22:24:42 +0500

    making some kind of data base

--- PIS_WinForm/GlobalClasses/StaticClasses/PermissionGuard.cs
+++ PIS_WinForm/GlobalClasses/StaticClasses/PermissionGuard.cs
@@ -4,18 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PIS_WinForm.GlobalClasses.StaticClasses
+namespace PIS_WinForm
 {
     public static class PermissionGuard
     {
 
-        public static bool CanLookAll() => throw new NotImplementedException();
+        public static bool CanLookAll(string table) => throw new NotImplementedException();
 
-        public static bool CanAdd() => throw new NotImplementedException();
+        public static bool CanAdd(string table) => throw new NotImplementedException();
 
-        public static bool CanEdit() => throw new NotImplementedException();
+        public static bool CanEdit(string table) => throw new NotImplementedException();
 
-        public static string GetTown() => throw new NotImplementedException();
+        public static string GetTown(string table) => throw new NotImplementedException();
 
         public static void ValidateUser(string login, string password)
         {
