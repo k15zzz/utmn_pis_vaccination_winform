d22d9ba8252d1b9df6379dd17160ad2aefec5675
Koptev Danil <99016838+k15zzz@users.noreply.github.com>
2023-06-02 02:42:36 +0500

    реализовал PermissionGuard.cs

--- PIS_WinForm/GlobalClasses/StaticClasses/PermissionGuard.cs
+++ PIS_WinForm/GlobalClasses/StaticClasses/PermissionGuard.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,18 +9,160 @@ namespace PIS_WinForm.GlobalClasses.StaticClasses
 {
     public static class PermissionGuard
     {
+        private static string _group;
+        private static string _town = null;
 
-        public static bool CanLookAll() => throw new NotImplementedException();
-
-        public static bool CanAdd() => throw new NotImplementedException();
-
-        public static bool CanEdit() => throw new NotImplementedException();
-
-        public static string GetTown() => throw new NotImplementedException();
-
-        public static void ValidateUser(string login, string password)
+        private static Dictionary<string, Dictionary<string, string>> _permissions = 
+            new Dictionary<string, Dictionary<string, string>>()
         {
-            DBAdapter.SearchUser(login, password);
-        }
+                { 
+                    "Куратор ВетСлужбы", 
+                    new Dictionary<string, string>() 
+                    {
+                        { "Животных",       "Просмотр" },
+                        { "Организаций",    "Просмотр" },
+                        { "МунКонтрактов",  "Просмотр" }
+                    } 
+                },
+                {
+                    "Куратор по отлову",
+                    new Dictionary<string, string>()
+                    {
+                        { "Животных",       "Просмотр" },
+                        { "Организаций",    "Просмотр" },
+                        { "МунКонтрактов",  "Просмотр" }
+                    }
+                },
+                {
+                    "Куратор приюта",
+                    new Dictionary<string, string>()
+                    {
+                        { "Животных",       "Просмотр" },
+                        { "Организаций",    "Просмотр" },
+                        { "МунКонтрактов",  "Просмотр" }
+                    }
+                },
+                {
+                    "Оператор ВетСлужбы",
+                    new Dictionary<string, string>()
+                    {
+                        { "Животных",       "Просмотр" },
+                        { "Организаций",    "Введение" },
+                        { "МунКонтрактов",  "Просмотр" }
+                    }
+                },
+                {
+                    "Оператор по Отлову",
+                    new Dictionary<string, string>()
+                    {
+                        { "Животных",       "Просмотр" },
+                        { "Организаций",    "Нет доступа" },
+                        { "МунКонтрактов",  "Нет доступа" }
+                    }
+                },
+                {
+                    "Подписант ВетСлужбы",
+                    new Dictionary<string, string>()
+                    {
+                        { "Животных",       "Просмотр" },
+                        { "Организаций",    "Просмотр" },
+                        { "МунКонтрактов",  "Просмотр" }
+                    }
+                },
+                {
+                    "Подписант по отлову",
+                    new Dictionary<string, string>()
+                    {
+                        { "Животных",       "Просмотр" },
+                        { "Организаций",    "Просмотр" },
+                        { "МунКонтрактов",  "Просмотр" }
+                    }
+                },
+                {
+                    "Подписант приюта",
+                    new Dictionary<string, string>()
+                    {
+                        { "Животных",       "Просмотр" },
+                        { "Организаций",    "Просмотр" },
+                        { "МунКонтрактов",  "Просмотр" }
+                    }
+                },
+                {
+                    "Куратор ОМСУ",
+                    new Dictionary<string, string>()
+                    {
+                        { "Животных",       "Просмотр" },
+                        { "Организаций",    "Просмотр" },
+                        { "МунКонтрактов",  "Просмотр" }
+                    }
+                },
+                {
+                    "Оператор ОМСУ",
+                    new Dictionary<string, string>()
+                    {
+                        { "Животных",       "Просмотр" },
+                        { "Организаций",    "Введение" },
+                        { "МунКонтрактов",  "Введение" }
+                    }
+                },
+                {
+                    "Подписант ОМСУ",
+                    new Dictionary<string, string>()
+                    {
+                        { "Животных",       "Просмотр" },
+                        { "Организаций",    "Просмотр" },
+                        { "МунКонтрактов",  "Просмотр" }
+                    }
+                },
+                {
+                    "Оператор приюта",
+                    new Dictionary<string, string>()
+                    {
+                        { "Животных",       "Введение" },
+                        { "Организаций",    "Нет доступа" },
+                        { "МунКонтрактов",  "Нет доступа" }
+                    }
+                },
+                {
+                    "filter",
+                    new Dictionary<string, string>()
+                    {
+                        { "Животных",       "Введение" },
+                        { "Организаций",    "Нет доступа" },
+                        { "МунКонтрактов",  "Нет доступа" }
+                    }
+                },
+                {
+                    "Ветврач приюта",
+                    new Dictionary<string, string>()
+                    {
+                        { "Животных",       "Введение" },
+                        { "Организаций",    "Нет доступа" },
+                        { "МунКонтрактов",  "Нет доступа" }
+                    }
+                }
+        };
+
+        public static void SetGroup(string group) => _group = group;
+
+        public static void SetTown(string town) => _town = town;
+
+        public static bool CanLookAll(string nameReestr) => GetPermissionVisable(nameReestr);
+
+        public static bool CanAdd(string nameReestr) => GetPermissionAction(nameReestr);
+
+        public static bool CanEdit(string nameReestr) => GetPermissionAction(nameReestr);
+
+        public static bool CanDelete(string nameReestr) => GetPermissionAction(nameReestr);
+
+        public static string GetTown() => _town;
+
+        public static void ValidateUser(string login, string password) => DBAdapter.SearchUser(login, password);
+
+        private static bool GetPermissionAction(string nameReestr) => GetPermission(nameReestr) == "Введение";
+
+        private static bool GetPermissionVisable(string nameReestr) => GetPermission(nameReestr) == "Просмотр";
+
+        private static string GetPermission(string nameReestr) => _permissions[_group][nameReestr];
     }
 }
