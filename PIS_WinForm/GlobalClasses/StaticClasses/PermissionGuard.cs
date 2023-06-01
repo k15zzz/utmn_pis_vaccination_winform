15eb2ecb254b53be85c944a3eab92fcf3f634102
Koptev Danil <99016838+k15zzz@users.noreply.github.com>
2023-06-02 00:41:55 +0500

    Переместил PermissionGuard.cs и удалил PermissionManager.cs

---
+++ PIS_WinForm/GlobalClasses/StaticClasses/PermissionGuard.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIS_WinForm.GlobalClasses.StaticClasses
+{
+    public static class PermissionGuard
+    {
+
+        public static bool CanLookAll() => throw new NotImplementedException();
+
+        public static bool CanAdd() => throw new NotImplementedException();
+
+        public static bool CanEdit() => throw new NotImplementedException();
+
+        public static string GetTown() => throw new NotImplementedException();
+
+        public static void ValidateUser(string login, string password)
+        {
+            DBAdapter.SearchUser(login, password);
+        }
+    }
+}
